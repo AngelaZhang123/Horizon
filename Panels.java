aea67d918e741635afd58d88b5b42de1245abb99
AngelaZhang123 <70032379+AngelaZhang123@users.noreply.github.com>
2020-10-10 22:02:12 -0700

    Add files via upload

--- Panels.java
+++ Panels.java
@@ -8,6 +8,9 @@ class Panels extends JPanel //where all the panels are created
 
     final static String MMPANEL = "MainMenu";
     final static String QPANEL = "QuizPanel";
+    final static String EPANEL = "EconomyP";
+    final static String IPANEL = "ImmigrationP";
+
     public Panels() //things are initialized here
     {
         lay = new CardLayout();
@@ -23,8 +26,13 @@ class Panels extends JPanel //where all the panels are created
     public void create() //the instances of the cards/panels are created and added to the cards
     {
         MainMenu mm = new MainMenu(lay,cards);
-        //Quiz qq = new Quiz();
+        Quiz qq = new Quiz(lay,cards);
+        Econ ee = new Econ(lay,cards);
+        Imm ii = new Imm(lay,cards);
+
         cards.add(mm,MMPANEL);
-        //cards.add(qq,QPANEL);
+        cards.add(qq,QPANEL);
+        cards.add(ee,EPANEL);
+        cards.add(ii,IPANEL);
     }
 }
--- Quiz.java
+++ Quiz.java
@@ -15,88 +15,100 @@ public class Quiz extends JPanel implements ActionListener{
     JPanel cards;
     int topic;
 
-    JButton sagree;
-    JButton neutral;
     JButton agree;
     JButton disagree;
-    JButton sdis;
+    JLabel q2;
+    JLabel q2a;
+
     public Quiz(CardLayout x, JPanel y)
     {
         lay =x;
         cards=y;
         topic =0;
 
-        JTextField txtInput = new JTextField("Which topic would you like to explore?");
-        add(txtInput);
+        Font font = new Font("Monospaced", Font.PLAIN, 30);
+        Font smallF = new Font("Monospaced", Font.PLAIN, 20);
+        Font bigF = new Font("Monospaced", Font.PLAIN, 25);
+
+        GridLayout grid = new GridLayout(2,0);
+        setLayout(grid);
+        JPanel top = new JPanel();
+        JPanel bottom = new JPanel();
+        add(top);
+        add(bottom);
+
+        JLabel filler = new JLabel("             ");
+        filler.setFont(bigF);
+        top.add(filler);
+
+        JLabel q1 = new JLabel("Which topic would you like to explore?");
+        q1.setFont(smallF);
+        top.add(q1);
         JButton one = new JButton("Economy");
-        JButton two = new JButton("Immigration");
-        JButton submit = new JButton("Submit");
+        JButton two = new JButton("Education");
 
         one.addActionListener(this);
-        one.setFont(new Font("Monospaced", Font.PLAIN, 30));
-        add(one);
+        one.setFont(font);
+        top.add(one);
         two.addActionListener(this);
-        two.setFont(new Font("Monospaced", Font.PLAIN, 30));
-        add(two);
-        submit.addActionListener(this);
-        submit.setFont(new Font("Monospaced", Font.PLAIN, 30));
-        add(submit);
+        two.setFont(font);
+        top.add(two);
 
-        /*sagree = new JButton("Strongly agree");
+        q2 = new JLabel("");
+        q2.setFont(font);
+        bottom.add(q2);
+        q2.setVisible(false);
+
+        q2a = new JLabel("");
+        q2a.setFont(font);
+        bottom.add(q2a);
+        q2a.setVisible(false);
+
         agree = new JButton("Agree");
-        neutral = new JButton("Neutral");
         disagree = new JButton("Disagree");
-        sdis = new JButton("Strongly Disagree");
 
-        add(sagree);
-        add(agree);
-        add(neutral);
-        add(disagree);
-        add(sdis);*/
+        bottom.add(agree);
+        bottom.add(disagree);
+        agree.setFont(font);
+        disagree.setFont(font);
+        agree.setVisible(false);
+        disagree.setVisible(false);
 
-        JSlider opinion=new JSlider(JSlider.HORIZONTAL, 5);
-        Hashtable hashTable = new Hashtable(4);
-
-        hashTable.put(new Integer( 0 ), new JLabel("Strongly Agree") );
-        hashTable.put(new Integer( 1 ), new JLabel("Agree") );
-        hashTable.put(new Integer( 2 ), new JLabel("Neutral") );
-        hashTable.put(new Integer( 3 ), new JLabel("Diagree") );
-        hashTable.put(new Integer( 4 ), new JLabel("Strongly Disagree") );
-
-        OpListener lst = new OpListener();
-
-        opinion.setLabelTable(hashTable);
-        opinion.setPaintLabels(true);
-        opinion.setMajorTickSpacing(1);
-        opinion.setPaintTicks(true);
-        opinion.addChangeListener(lst);
-        add(opinion);
+        top.setBackground(new Color(255,204,51));
+        bottom.setBackground(new Color(255,204,51));
     }
     public void paintComponent(Graphics g) //the graphics for Main menu (the background pic)
     {
         super.paintComponent(g);
+
     }
     public void actionPerformed(ActionEvent e)
     {
         String command = e.getActionCommand();
         if(command.equals("Economy")) {
             topic = 1;
+            q2.setText("There should be more government regulations");
+            q2a.setText("put on larger businesses and corporations.");
+            q2.setVisible(true);
+            q2a.setVisible(true);
+            agree.setVisible(true);
+            disagree.setVisible(true);
             repaint();
         }
-        if(command.equals("Immigration")) {
+        if(command.equals("Education")) {
             topic = 2;
+            q2.setText("The public education system is flawed ");
+            q2a.setText("and should be defunded.");
+            q2.setVisible(true);
+            q2a.setVisible(true);
+            agree.setVisible(true);
+            disagree.setVisible(true);
             repaint();
         }
         if(command.equals("Submit"))
         {
             if(topic==1)lay.show(cards,"EconomyP");
             else lay.show(cards,"ImmigrationP");
-        }
-    }
-    class OpListener implements ChangeListener
-    {
-        public void stateChanged(ChangeEvent e) {
-
         }
     }
 }
