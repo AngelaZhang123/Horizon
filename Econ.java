45478cef864323813a80939e122827028b7e5680
AngelaZhang123 <70032379+AngelaZhang123@users.noreply.github.com>
2020-10-10 23:22:19 -0700

    Add files via upload

--- Econ.java
+++ Econ.java
@@ -18,6 +18,20 @@ public class Econ extends JPanel implements ActionListener{
         back.addActionListener(this);
         setLayout(new BorderLayout());
         add(back,BorderLayout.SOUTH);
+
+        JLabel title = new JLabel("ECONOMY");
+        title.setFont(new Font("Monospaced", Font.PLAIN, 80));
+        add(title,BorderLayout.NORTH);
+
+        String info = "Supply-side economics: increased production drives economic growth. \nThe factors of production are capital, labor, entrepreneurship, and land";
+        info+="\nTax cuts on businesses/corps and deregulation (lower minimum wage, \nless regulation on workers’ benefits, anti-workers union(?)\n";
+        info+="\nThat says what's good for the wealthy will trickle down to everyone \nin the society. Proponents believe that investors, savers, \nand company owners are the real drivers of growth.\n";
+        info+="\nAdvocates of trickle-down economics promise that businesses will use \nthe extra cash from tax cuts to expand. Investors will use \ntheir tax-cut windfall to buy more companies or \nstocks. Owners will invest in their operations and hire workers.4\n";
+        info+="\nSupply-siders claim that this greater growth will always make up for \n the lost tax revenue.";
+
+        JTextArea body = new JTextArea(info);
+        body.setFont(new Font("Monospaced", Font.PLAIN, 10));
+        add(body,BorderLayout.CENTER);
     }
     public void paintComponent(Graphics g) //the graphics for Main menu (the background pic)
     {
